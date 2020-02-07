c4de1d79a172c4bd5d4281f18515c5dda4160d2e
SlothStyleKyle <43865958+SlothStyleKyle@users.noreply.github.com>
2020-02-06 22:14:12 -0600

    Create videoGameBot.py
    Adding file.
    makes a call to igdb using unix timestamp for todays date.
    sorts the returned JSON for game names and platforms, prints to console for now

---
+++ videoGameBot.py
@@ -0,0 +1,49 @@
+import os
+import json
+import requests
+from datetime import datetime, date
+import calendar
+import discord
+
+class Game:
+    def __init__(self, name):
+        self.name = name
+        self.platforms=[]
+
+    def addPlatform(self,platform):
+        self.platforms.append(platform)
+
+currentTime=datetime.now()
+
+d = datetime(currentTime.year,currentTime.month,currentTime.day,0,0)
+
+timestamp=calendar.timegm(d.timetuple())
+
+URL ="https://api-v3.igdb.com/release_dates"
+hdrs = {'user-key': 'YOUR IGDB KEY HERE'}
+payload='fields game.name, platform.name; where date='+str(timestamp)+';'
+
+r=requests.post(url=URL, headers=hdrs, data=payload)
+data=r.json
+resp=r.text
+
+resp_string = json.loads(resp)
+
+games=[]
+
+for i in resp_string:
+    games.append(i["game"]["name"])
+
+game=list(set(games))
+
+temp=[]
+for i in range(len(game)):
+    temp.append(Game(game[i]))
+    for j in resp_string:
+        if game[i]==j["game"]["name"]:
+            temp[i].addPlatform(j["platform"]["name"])
+    
+for i in range(len(temp)):
+    print(temp[i].name)
+    print(temp[i].platforms)
+    print("")
