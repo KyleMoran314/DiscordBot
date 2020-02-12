e6b1a666e98bc72e912bcab20e127fe2ca310c1d
kymoran <kymoran@siue.edu>
2020-02-11 21:21:55 -0600

    added discord funtionality

--- videoGameBot.py
+++ videoGameBot.py
@@ -53,3 +53,30 @@ for i in range(len(temp)):
     print(temp[i].platforms)
     print(temp[i].gamedate)
     print("")
+
+TOKEN = 'YOUR DISCORD TOKEN HERE'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.author.send("Games released today are:")
+        for i in range(len(temp)):
+            game=temp[i].name
+            platforms=temp[i].platforms
+            await message.author.send(game)
+            await message.author.send(platforms)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
